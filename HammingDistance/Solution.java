6884c05d1e18e20daa022b585bd2b432230b9adc
Sujal Bhavsar <sujalofficial011@gmail.com>
2024-11-10 15:33:58 +0530

    Boardgames solution added

--- HammingDistance/Solution.java
+++ HammingDistance/Solution.java
@@ -16,12 +16,14 @@ class Solution{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
+        String[] input = new String[n];
         // String[] array = new String[n];
         for (int i = 0; i < n; i++) {
-            String bitString = sc.nextLine();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            input[i]=sc.nextLine();
             sc.nextLine();
+        }
+        for (int i = 0; i < n; i++) {
+            String bitString = input[i];
             int countZero = 0;
             int countOne = 0;
             boolean invalidFlag=false;
@@ -36,12 +38,13 @@ class Solution{
 
             }
             if (invalidFlag) {
-                System.out.println("INVALID");
+                System.out.print("\n"+"INVALID");
                 continue;
             }
             StringBuilder reString1 = new StringBuilder();
             StringBuilder reString2 = new StringBuilder();
 
+            // java 11
             // reString1.append("0".repeat(countZero));
             // reString1.append("1".repeat(countOne));
             // reString2.append("1".repeat(countOne));
